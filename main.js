68c2a4940fa1437aa229c4acd7fa01982829ffb8
ayesha <163387252+Ayeshaaaqil@users.noreply.github.com>
2024-04-22 21:06:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, //base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter from currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "to",
+        message: "Enter from currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "amount",
+        message: "Enter from amount",
+        type: "number",
+    }
+]);
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount; //USD base currency //4
+let convertedAmount = baseAmount * toAmount;
+console.log(user_answer.from);
+console.log(user_answer.to);
+console.log(convertedAmount);
+//display the converted amount
+console.log(`Converted Amount =${convertedAmount.toFixed(2)}`);
